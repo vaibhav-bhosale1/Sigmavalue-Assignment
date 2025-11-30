ef6fc3979864e42e98154df524821cb40e598056
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-11-30 13:39:14 +0530

    refactor: remove migration command from build script; will run in Start Command instead

--- realestate_chatbot/build.sh
+++ realestate_chatbot/build.sh
@@ -8,5 +8,4 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Run migrations
-python manage.py migrate
+# REMOVED: python manage.py migrate (We will run this in the Start Command instead)
